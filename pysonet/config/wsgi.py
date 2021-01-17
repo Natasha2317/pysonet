6ee1471497b2e2c27167264e1bbd002bec487060
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2021-01-18 01:54:48 +0300

    Update wsgi.py

--- pysonet/config/wsgi.py
+++ pysonet/config/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+# add the hellodjango project path into the sys.path
+sys.path.append('<PATH_TO_MY_DJANGO_PROJECT>/config')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('<PATH_TO_VIRTUALENV>/Lib/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
