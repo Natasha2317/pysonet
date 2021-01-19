5af2e4c5241072db29ecb101abff38913d3eff3f
Natasha2317 <nataashaa006@gmail.com>
2021-01-19 17:06:19 +0300

    style

--- pysonet/config/settings.py
+++ pysonet/config/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 from datetime import timedelta
 import mimetypes
-mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
