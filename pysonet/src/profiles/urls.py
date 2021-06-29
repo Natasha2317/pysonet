e771dd1718a04543c74a4bfde7baeac5fcb02961
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2021-06-29 21:52:57 +0300

    Update urls.py

--- pysonet/src/profiles/urls.py
+++ pysonet/src/profiles/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile/<int:pk>/', views.UserNetPrivateView.as_view({'get': 'retrieve', 'put': 'update'})),
-    path('<int:pk>/', views.UserNetPublicView.as_view({'get': 'retrieve'})),
+    path('profile/', views.UserNetPrivateView.as_view({'get': 'retrieve', 'put': 'update'})),
+    path('profile/<int:pk>/', views.UserNetPublicView.as_view({'get': 'retrieve'})),
 
 ]
