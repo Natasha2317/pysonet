a98fe04fe5cfdbe85335bb9325acfd52439cab1e
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2021-06-30 00:46:55 +0300

    Update urls.py

--- pysonet/src/profiles/urls.py
+++ pysonet/src/profiles/urls.py
@@ -1,11 +1,7 @@
 
-from django.urls import path
-from . import views
-
-
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('profile/', views.UserNetPrivateView.as_view({'get': 'retrieve', 'put': 'update'})),
+    path('profile/', views.UserNetPrivateView.as_view()),
+    # path('profile/avatar/', views.UserNetAvatarView.as_view()),
+    # path('profile/avatar/base64/', views.UserNetAvatarBase64View.as_view()),
     path('profile/<int:pk>/', views.UserNetPublicView.as_view({'get': 'retrieve'})),
-
 ]
