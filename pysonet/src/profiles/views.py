25409ff73bb74159850a4fdebf8c723455eddc6d
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2021-06-30 00:46:02 +0300

    Update views.py

--- pysonet/src/profiles/views.py
+++ pysonet/src/profiles/views.py
@@ -1,11 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions, generics, response, mixins
+from django.shortcuts import get_object_or_404
 
 from .models import UserNet
 from .serializers import GetUserNetSerializer, GetUserNetPublicSerializer
 
 from django.http import HttpResponse
-
 
 def index(request):
     return HttpResponse("Это страница заглушка.")
@@ -17,7 +17,7 @@ class UserNetPublicView(ModelViewSet):
     serializer_class = GetUserNetPublicSerializer
     permission_classes = [permissions.AllowAny]
 
-class UserNetPrivateView(ModelViewSet):
+class UserNetPrivateView(generics.RetrieveUpdateAPIView):
     """ Вывод профиля пользователя
     """
     serializer_class = GetUserNetSerializer
@@ -25,3 +25,8 @@ class UserNetPrivateView(ModelViewSet):
 
     def get_queryset(self):
         return UserNet.objects.filter(id=self.request.user.id)
+
+    def get_object(self):
+        obj = get_object_or_404(self.get_queryset())
+        self.check_object_permissions(self.request, obj)
+        return obj
