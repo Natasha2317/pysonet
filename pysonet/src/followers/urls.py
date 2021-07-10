7261e9e4f99e6ee86946f85e5eaaf0fad9285af4
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2021-07-10 22:41:02 +0300

    Update urls.py

--- pysonet/src/followers/urls.py
+++ pysonet/src/followers/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>', views.FollowerView.as_view()),
-    path('', views.ListFollowerView.as_view()),
-    path('/<int:pk>', views.FollowerView.as_view()),
+    path('', views.ListFollowerView.as_view())
 
 ]
