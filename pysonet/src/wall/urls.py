1bf94e599e2e873f1deb4a2749d65c4eddf9e61b
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2021-07-14 00:26:08 +0300

    Update urls.py

--- pysonet/src/wall/urls.py
+++ pysonet/src/wall/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('post/<int:pk>', views.PostView.as_view({
         'get': 'retrieve', 'put': 'update', 'delete': 'destroy'
     })),
-    path('<int:pk>', views.PostListView.as_view()),
+    path('post/<int:pk>', views.PostListView.as_view()),
 ]
 
 
